346e555bb389323c7af9e8664fc0ffe7aac0e982
justvinny <vinsonemb.151994@gmail.com>
2022-06-05 16:46:39 +1200

    WIP - Login UI

---
+++ app/src/main/java/com/vinsonb/password/manager/kotlin/fragments/LoginFragment.kt
@@ -0,0 +1,8 @@
+package com.vinsonb.password.manager.kotlin.fragments
+
+import androidx.fragment.app.Fragment
+import com.vinsonb.password.manager.kotlin.R
+
+class LoginFragment : Fragment(R.layout.fragment_login) {
+
+}
