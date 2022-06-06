09412ce796745eac0df41b4c28b18f50d4cdfaaf
justvinny <vinsonemb.151994@gmail.com>
2022-06-06 18:51:32 +1200

    Added Unit and UI tests

--- app/src/androidTest/java/com/vinsonb/password/manager/kotlin/fragments/CreateLoginFragmentTest.kt
+++ app/src/androidTest/java/com/vinsonb/password/manager/kotlin/fragments/CreateLoginFragmentTest.kt
@@ -1,21 +1,137 @@
 package com.vinsonb.password.manager.kotlin.fragments
 
-import androidx.fragment.app.testing.launchFragment
+import android.content.Context
+import android.view.View
+import androidx.fragment.app.testing.FragmentScenario
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.lifecycle.Lifecycle
+import androidx.navigation.Navigation
+import androidx.navigation.testing.TestNavHostController
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.google.android.material.textfield.TextInputLayout
 import com.vinsonb.password.manager.kotlin.R
+import org.hamcrest.CoreMatchers.not
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.TypeSafeMatcher
+import org.junit.After
+import org.junit.Assert
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class CreateLoginFragmentTest {
+    private lateinit var scenario: FragmentScenario<CreateLoginFragment>
+    private lateinit var targetContext: Context
+
+    @Before
+    fun setup() {
+        scenario =
+            launchFragmentInContainer(themeResId = R.style.Theme_PasswordManagerKotlinRewrite)
+        scenario.moveToState(Lifecycle.State.STARTED)
+        targetContext = InstrumentationRegistry.getInstrumentation().targetContext
+    }
+
+    @After()
+    fun teardown() {
+        scenario.moveToState(Lifecycle.State.DESTROYED)
+        scenario.close()
+    }
 
     @Test
     fun passcode_textInput_given5DigitsEntered_showsNoError() {
-        val scenario = launchFragment<CreateLoginFragment>()
+        onView(withId(R.id.passcode_text))
+            .perform(typeText("55555"))
+
+        Espresso.closeSoftKeyboard()
+
         onView(withId(R.id.passcode))
-            .perform(replaceText("55555"))
+            .check(matches(not(hasTextInputLayoutHintText(targetContext.resources.getString(R.string.error_passcode_length)))))
     }
+
+    @Test
+    fun passcode2_textInput_givenMatchingPasscode_showsNoError() {
+        onView(withId(R.id.passcode_text))
+            .perform(typeText("55555"))
+
+        onView(withId(R.id.passcode_2_text))
+            .perform(typeText("55555"))
+
+        Espresso.closeSoftKeyboard()
+
+        onView(withId(R.id.passcode_2))
+            .check(matches(not(hasTextInputLayoutHintText(targetContext.resources.getString(R.string.error_passcode_must_match)))))
+    }
+
+    @Test
+    fun secretQuestion_textInput_givenText_showsNoError() {
+        onView(withId(R.id.secret_question_text))
+            .perform(typeText("Question"))
+
+        Espresso.closeSoftKeyboard()
+
+        onView(withId(R.id.secret_question))
+            .check(matches(not(hasTextInputLayoutHintText(targetContext.resources.getString(R.string.error_text_empty)))))
+    }
+
+    @Test
+    fun secretAnswer_textInput_givenText_showsNoError() {
+        onView(withId(R.id.secret_answer_text))
+            .perform(typeText("Answer"))
+
+        Espresso.closeSoftKeyboard()
+
+        onView(withId(R.id.secret_answer))
+            .check(matches(not(hasTextInputLayoutHintText(targetContext.resources.getString(R.string.error_text_empty)))))
+    }
+
+    @Test
+    fun buttonCreateLogin_clickEvent_givenValidData_navigatesToLoginFragment() {
+        val navController = TestNavHostController(ApplicationProvider.getApplicationContext())
+        scenario.onFragment {
+            navController.setGraph(R.navigation.nav_graph)
+            navController.setCurrentDestination(R.id.createLoginFragment)
+            Navigation.setViewNavController(it.requireView(), navController)
+        }
+
+        onView(withId(R.id.passcode_text))
+            .perform(typeText("55555"))
+
+        onView(withId(R.id.passcode_2_text))
+            .perform(typeText("55555"))
+
+        onView(withId(R.id.secret_question_text))
+            .perform(typeText("Question"))
+
+        onView(withId(R.id.secret_answer_text))
+            .perform(typeText("Answer"))
+
+        Espresso.closeSoftKeyboard()
+
+        onView(withId(R.id.button_create_login))
+            .perform(click())
+
+        Assert.assertEquals(navController.currentDestination?.id, R.id.loginFragment)
+    }
+
+    private fun hasTextInputLayoutHintText(expectedErrorText: String): Matcher<View> =
+        object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description?) {}
+
+            override fun matchesSafely(item: View?): Boolean {
+                if (item !is TextInputLayout) return false
+                val error = item.hint ?: return false
+                val hint = error.toString()
+                return expectedErrorText == hint
+            }
+        }
 }
--- app/src/androidTest/java/com/vinsonb/password/manager/kotlin/fragments/LoginFragmentTest.kt
+++ app/src/androidTest/java/com/vinsonb/password/manager/kotlin/fragments/LoginFragmentTest.kt
@@ -1,4 +1,69 @@
 package com.vinsonb.password.manager.kotlin.fragments
 
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.fragment.app.testing.FragmentScenario
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.lifecycle.Lifecycle
+import androidx.navigation.Navigation
+import androidx.navigation.testing.TestNavHostController
+import androidx.preference.PreferenceManager
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.platform.app.InstrumentationRegistry
+import com.vinsonb.password.manager.kotlin.Constants
+import com.vinsonb.password.manager.kotlin.R
+import org.junit.After
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
 class LoginFragmentTest {
+    private lateinit var scenario: FragmentScenario<LoginFragment>
+    private lateinit var targetContext: Context
+    private lateinit var preferences: SharedPreferences
+
+    @Before
+    fun setup() {
+        scenario =
+            launchFragmentInContainer(themeResId = R.style.Theme_PasswordManagerKotlinRewrite)
+        scenario.moveToState(Lifecycle.State.STARTED)
+        targetContext = InstrumentationRegistry.getInstrumentation().targetContext
+        preferences = PreferenceManager.getDefaultSharedPreferences(targetContext)
+    }
+
+    @After()
+    fun teardown() {
+        scenario.moveToState(Lifecycle.State.DESTROYED)
+        scenario.close()
+    }
+
+    @Test
+    fun validPassword_entered_navigateToHomeFragment() {
+        with(preferences.edit()) {
+            putString(Constants.Companion.Password.SharedPreferenceKeys.PASSCODE_KEY, "55555")
+            apply()
+        }
+
+        val navController = TestNavHostController(ApplicationProvider.getApplicationContext())
+        scenario.onFragment {
+            navController.setGraph(R.navigation.nav_graph)
+            navController.setCurrentDestination(R.id.loginFragment)
+            Navigation.setViewNavController(it.requireView(), navController)
+        }
+
+        repeat(5) {
+            Espresso.onView(ViewMatchers.withId(R.id.button_5))
+                .perform(ViewActions.click())
+        }
+
+        Assert.assertEquals(navController.currentDestination?.id, R.id.viewAccountsFragment)
+
+        with(preferences.edit()) {
+            clear()
+            apply()
+        }
+    }
 }
