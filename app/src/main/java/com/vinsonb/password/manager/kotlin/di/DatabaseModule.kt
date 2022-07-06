7d6e53a50e976711a3b2ab9f3cf18ae6967c480c
Vinson Beduya <vinsonemb.151994@gmail.com>
2022-07-06 17:00:34 +1200

    Added SQLCipher for Room

--- app/src/main/java/com/vinsonb/password/manager/kotlin/di/DatabaseModule.kt
+++ app/src/main/java/com/vinsonb/password/manager/kotlin/di/DatabaseModule.kt
@@ -2,6 +2,7 @@ package com.vinsonb.password.manager.kotlin.di
 
 import android.content.Context
 import androidx.room.Room
+import com.vinsonb.password.manager.kotlin.R
 import com.vinsonb.password.manager.kotlin.database.AccountLocalDatabase
 import com.vinsonb.password.manager.kotlin.database.AccountRepository
 import com.vinsonb.password.manager.kotlin.utilities.Constants
@@ -10,6 +11,7 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import net.sqlcipher.database.SupportFactory
 import javax.inject.Singleton
 
 @Module
@@ -17,14 +19,19 @@ import javax.inject.Singleton
 object DatabaseModule {
     @Singleton
     @Provides
-    fun providesAccountLocalDatabase(@ApplicationContext context: Context): AccountLocalDatabase =
-        Room
+    fun providesAccountLocalDatabase(@ApplicationContext context: Context): AccountLocalDatabase {
+        val passphrase = context.getString(R.string.passphrase)
+        val factory = SupportFactory(passphrase.toByteArray())
+        return Room
             .databaseBuilder(
                 context, AccountLocalDatabase::class.java,
                 Constants.Database.DATABASE_NAME
             )
+            .openHelperFactory(factory)
             .fallbackToDestructiveMigration()
             .build()
+    }
+
 
     @Singleton
     @Provides
